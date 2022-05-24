de61bc1803b61140be93b29225fd210cc576f464
Parker, Michael <michael.parker2@sky.uk>
2022-05-24 16:19:04 +0100

    Code refactor

--- tests/first.spec.js
+++ tests/first.spec.js
@@ -13,15 +13,13 @@ test('Search Sky Glass', async({page}) => {
     //Click the search glass
     await page.locator('data-test-id=masthead-search-toggle-button').click();
 
-    //Fill the input box in search and fill
+    //Fill the input box and click the search icon
     await page.locator('data-test-id=input-box').fill('Glass');
+    await page.waitForTimeout(2000);
     await page.locator('data-test-id=submit-button').click();
 
     //Confirm the page loads correctly
     const searchResult = page.locator('.c-editorial-layer__title');
     await expect(searchResult).toHaveText('Sky Glass');   
-    
-    //Close the browser
-    await browser.close();
 
-})();
+});
